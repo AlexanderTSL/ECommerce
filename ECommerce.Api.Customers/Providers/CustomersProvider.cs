b52f231af1a07e071ec77e9b35a6deaa4d91d764
Киевский бандит Александр <sanyokpenyok@yahoo.com>
2023-06-02 19:52:53 +0300

    Добавьте файлы проекта.

---
+++ ECommerce.Api.Customers/Providers/CustomersProvider.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using ECommerce.Api.Customers.Db;
+using ECommerce.Api.Customers.Interfaces;
+using ECommerce.Api.Customers.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Customers.Providers
+{
+    public class CustomersProvider : ICustomersProvider
+    {
+        private readonly CustomerDbContext context;
+        private readonly ILogger<CustomersProvider> logger;
+        private readonly IMapper mapper;
+
+        public CustomersProvider(CustomerDbContext context, ILogger<CustomersProvider> logger, IMapper mapper)
+        {
+            this.context = context;
+            this.logger = logger;
+            this.mapper = mapper;
+
+            SeedData();
+        }
+
+        private void SeedData()
+        {
+            if(!context.Customers.Any())
+            {
+                context.Customers.Add(new Db.Customer {Id = 1, Name = "Alex", Address = "John Kingksley 3"} );
+                context.Customers.Add(new Db.Customer {Id = 2, Name = "Nick", Address = "Burming 5"} );
+                context.Customers.Add(new Db.Customer {Id = 3, Name = "Jake", Address = "Burkena 10"} );
+                context.Customers.Add(new Db.Customer {Id = 4, Name = "Michael", Address = "Bornmutt 15"} );
+                context.SaveChanges();
+            }
+        }
+
+        public async Task<(bool isSuccess, Models.Customer Customer, string ErrorMessage)> GetCustomerAsync(int id)
+        {
+            try
+            {
+                var customer = await context.Customers.FirstOrDefaultAsync(c => c.Id == id);
+                if (customer != null)
+                {
+                    var result = mapper.Map<Db.Customer, Models.Customer>(customer);
+                    return (true, result, null);
+                }
+                return (false, null, "Not Found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+
+        public async Task<(bool isSuccess, IEnumerable<Models.Customer> Customers, string ErrorMessage)> GetCustomersAsync()
+        {
+            try
+            {
+                var customers = await context.Customers.ToListAsync();
+                if (customers != null && customers.Any())
+                {
+                    var result = mapper.Map<IEnumerable<Db.Customer>, IEnumerable<Models.Customer>>(customers);
+                    return (true, result, null);
+                }
+                return (false, null, "Not Found");
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
---
+++ ECommerce.Api.Search/Interfaces/ICustomersService.cs
@@ -0,0 +1,12 @@
+﻿using ECommerce.Api.Search.Models;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Search.Interfaces
+{
+    public interface ICustomersService
+    {
+        Task<(bool IsSuccess, dynamic Customer, string ErrorMessage)> GetCustomerAsync(int id);
+    }
+}
